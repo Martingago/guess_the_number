8b5ef9384ce8d3a350655336a4ef53745a1e1f3b
Martin <gagochorenmartin@gmail.com>
2024-05-10 12:32:01 +0200

    añadida imagen de portada que cambia con el idioma + iconos en los botones

--- app/src/main/java/com/example/guess_number/fragments/MainMenuApplication.kt
+++ app/src/main/java/com/example/guess_number/fragments/MainMenuApplication.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.guess_number.R
+import java.util.Locale
 
 class MainMenuFragment : Fragment() {
 
@@ -19,7 +21,18 @@ class MainMenuFragment : Fragment() {
         // Inflar el diseño XML del fragmento
         val view = inflater.inflate(R.layout.fragment_main_menu_application, container, false)
 
-       //Botón de jugar
+       //Imagen de portada en sus 2 idiomas
+        val currentLocale = Locale.getDefault().language
+        val imageView = view.findViewById<ImageView>(R.id.imageView)
+
+        if (currentLocale == "es") {
+            imageView.setImageResource(R.drawable.logotipo_adivina_el_numero_esp)
+        } else {
+            imageView.setImageResource(R.drawable.logotipo_adivina_el_numero_eng)
+        }
+
+
+        //Botón de jugar
         val buttonJugar = view.findViewById<Button>(R.id.btnJugar)
         buttonJugar.setOnClickListener {
             view.findNavController().navigate(R.id.action_mainMenuFragment_to_gameFragment)
